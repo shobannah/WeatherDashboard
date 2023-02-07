cf55b81ad7a400981f1a0ffb3390bf526b162c91
shobannah <116910257+shobannah@users.noreply.github.com>
2023-02-07 00:17:59 -0500

    set columns using bootstrap

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,30 @@
+;
+var button = document.querySelector('.button');
+var search = document.querySelector('.search');
+var name = document.querySelector('.name');
+var date = document.querySelector('.date');
+var temp = document.querySelector('.temp');
+var wind = document.querySelector('.wind');
+var humidity = document.querySelector('.humidity');
+
 var APIKey = "ee1d17369c17fb2d95eb1b5c4433c89b";
 
-var city;
+var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?q="+ search.value +"&appid=" + APIKey;
 
-var requestUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+console.log(requestUrl)
 
-var searchInput = document.getElementById("search-input");
 
-// fetch(requestUrl)
-// 	.then(function (response) {
-// 		console.log(response.status);
-// 		return response.json();
-// 	})
-// 	.then(function (data) {
-// 		console.log(data);
-// });
 
-function getApi(requestUrl) {
+
+button.addEventListener('click', function() {
 	fetch(requestUrl)
-		.then(function (response) {
-			console.log(response.status);
-			return response.json();
+	.then(function (response) {
+		// console.log(response.data)
+		return response.json();
 		})
-		.then(function (data) {
-			console.log(data);
+	.then(function (data) {
+		console.log(data);
 		});
-}
+})
 
-getApi(requestUrl);
 
