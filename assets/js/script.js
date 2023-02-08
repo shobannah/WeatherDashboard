a790439be63f8feae65bdde502564ff759d258ed
shobannah <116910257+shobannah@users.noreply.github.com>
2023-02-07 20:52:13 -0500

    added 2 functions to get API info and render it on page-not working yet

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,47 @@
-;
+
 var button = document.querySelector('.button');
 var search = document.querySelector('.search');
-var name = document.querySelector('.name');
-var date = document.querySelector('.date');
-var temp = document.querySelector('.temp');
-var wind = document.querySelector('.wind');
-var humidity = document.querySelector('.humidity');
+// var name = document.querySelector('.name');
+// var date = document.querySelector('.date');
+// var temp = document.querySelector('.temp');
+// var wind = document.querySelector('.wind');
+// var humidity = document.querySelector('.humidity');
 
 var APIKey = "ee1d17369c17fb2d95eb1b5c4433c89b";
 
-var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?q="+ search.value +"&appid=" + APIKey;
+var cityName = [];
+var lat = [];
+var lon = [];
 
-console.log(requestUrl)
+function getAPI() {
 
+	var requestUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + search + "&appid=" + APIKey 
 
-
-
-button.addEventListener('click', function() {
 	fetch(requestUrl)
 	.then(function (response) {
-		// console.log(response.data)
+		console.log(response)
 		return response.json();
-		})
+	})
 	.then(function (data) {
 		console.log(data);
-		});
-})
+	})
+}
 
+button.addEventListener('click', getAPI())
 
+// function showWeather() {
+
+	
+// 	var url = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey
+
+// 	fetch(url)
+// 	.then(function (response) {
+// 		console.log(response)
+// 		return response.json();
+// 	})
+// 	.then(function (data) {
+// 		console.log(data);
+// 	})
+// }
+
+// showWeather()
